ed3dd9a99aad8831ada039e8a429465f7d87acbe
Abhishek Shujalpurkar <abhishekshujalpurkar@gmail.com>
2024-04-18 09:14:50 +0530

    17 april assignments

--- day6/assignment4.c
+++ day6/assignment4.c
@@ -1,6 +1,5 @@
 /*
 Q.4
-
 Find a partition point in array
 
 Given an unsorted array of integers. Find an element such that all the elements to its left are smaller and to its right are greater. Print -1 if no such element exists.
@@ -15,3 +14,18 @@ Output : 5
 Input : A[] = {5, 6, 2, 8, 10, 9, 8}
 Output : -1
 */
+
+#include<stdio.h>
+
+int findPoint(int arr[],int size)
+{
+    
+}
+
+int main()
+{
+    int  arr[] = {4, 3, 2, 5, 8, 6, 7};
+    int size= sizeof(arr)/sizeof(arr[0]);
+
+    printf("point at: %d",findPoint(arr,size));
+}
